a587537658e562dfeed06f68ba2ba5f251a2d528
enzo danjour <enzo.lops999@gmail.com>
2021-04-19 22:44:05 -0300

    change the file of homepage

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,17 @@
+import 'package:dquiz/home/widgets/app_bar/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
   }
 }
