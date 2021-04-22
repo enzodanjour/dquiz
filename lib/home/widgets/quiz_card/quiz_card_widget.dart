f312999006d8f0174cbed8eef12327048a471ea0
enzo danjour <enzo.lops999@gmail.com>
2021-04-21 22:36:08 -0300

    Add quiz card widget

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dquiz/core/app_colors.dart';
 import 'package:dquiz/core/app_text_styles.dart';
 import 'package:dquiz/core/core.dart';
+import 'package:dquiz/shared/widgets/progress/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -46,11 +47,7 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 2,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
+                child: ProgressIndicatorWidget(value: 0.3)
               )
             ],
           )
