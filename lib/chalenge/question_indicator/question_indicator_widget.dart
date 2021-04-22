410ac2b9e5105bed6ea82539fecd93294caaa373
enzo danjour <enzo.lops999@gmail.com>
2021-04-21 21:27:37 -0300

    Init question indicator

---
+++ lib/chalenge/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Questão 04"),
+
+        ],
+      ),
+    );
+  }
+}
