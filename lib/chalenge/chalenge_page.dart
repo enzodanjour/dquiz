174d576825cdc5d62e94adf1a5f785fa05ab7617
enzo danjour <enzo.lops999@gmail.com>
2021-04-24 02:55:43 -0300

    Add buttons in chalenge page

--- lib/chalenge/chalenge_page.dart
+++ lib/chalenge/chalenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:dquiz/chalenge/next_button/next_button.dart';
 import 'package:dquiz/chalenge/question_indicator/question_indicator_widget.dart';
 import 'package:dquiz/chalenge/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,20 @@ class _ChalengePageState extends State<ChalengePage> {
         child: SafeArea(child: QuestionIndicatorWidget()),
       ),
       body: QuizWidget(title: "O que o flutter faz em sua totalidade"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Fácil")),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
